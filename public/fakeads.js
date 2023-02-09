dc35dc11c7bb44b9437a3b8f2c905c8689473f98
Jaden Eckel <jadeneckel@gmail.com>
2023-02-09 16:27:44 -0600

    changing path

--- public/fakeads.js
+++ public/fakeads.js
@@ -3,7 +3,7 @@ function random_imglink(){
     var ry=Math.floor(Math.random()*30) //30 being the amount of images in the ads folder
     if (ry==0)
     ry=1
-    document.write('<a href="https://www.youtube.com/watch?v=phAKccY6M9I"><img src="/public/images/ads/' + ry + '.gif" style="max-width: 100%" border=0></a>');
+    document.write('<a href="https://www.youtube.com/watch?v=phAKccY6M9I"><img src="./images/ads/' + ry + '.gif" style="max-width: 100%" border=0></a>');
   }
 
 random_imglink()
